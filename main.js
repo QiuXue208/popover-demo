bb8c9c04c91eca15da3ddbc6653504926cc5e517
flower <1309571292@qq.com>
2018-08-06 11:01:04 +0800

    popover

---
+++ main.js
@@ -0,0 +1,14 @@
+$(clickMe).on('click',function(){
+  if($(popover).is(':hidden')){
+     $(popover).show()
+  }else{
+    $(popover).hide()
+  }
+  $(document).one('click',function(e){
+    $(popover).hide()
+  })
+
+})
+$(wrapper).on('click',function(e){
+  e.stopPropagation()
+})
